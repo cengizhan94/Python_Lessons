26ccde48158607380fba7008252398dcafd55916
Cengiz Han <sonsair11@gmail.com>
2023-07-07 19:48:46 +0300

    Pythagorean theorem

--- Dr.Chuck/Chapter1.py
+++ Dr.Chuck/Chapter1.py
@@ -4,9 +4,18 @@ fahrenheit = celsius + 32 * 1.8
 print(fahrenheit)"""
 
 #BMI
-mass = int(input("Your mass: "))
+"""mass = int(input("Your mass: "))
 height = float(input("Your height: "))
 
 BMI = mass/height ** 2
 
-print(BMI)
+print(BMI)"""
+
+#Pythagorean theorem
+a = int(input("length of short side 'a': "))
+b = int(input("length of short side 'b': "))
+
+c = (a**2 + b**2) **0.5
+
+print(c)
+
