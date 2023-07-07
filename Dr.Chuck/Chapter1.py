71ecd69b68c3b625aa5e5190b5ed322d1d747889
Cengiz Han <sonsair11@gmail.com>
2023-07-07 19:47:17 +0300

    Calculating BMI

--- Dr.Chuck/Chapter1.py
+++ Dr.Chuck/Chapter1.py
@@ -1,4 +1,12 @@
 #Celsius to Fahrenheit
-celsius = int(input("Celsius: "))
+"""celsius = int(input("Celsius: "))
 fahrenheit = celsius + 32 * 1.8
-print(fahrenheit)
+print(fahrenheit)"""
+
+#BMI
+mass = int(input("Your mass: "))
+height = float(input("Your height: "))
+
+BMI = mass/height ** 2
+
+print(BMI)
