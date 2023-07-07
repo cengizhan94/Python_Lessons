eea5872939d63cca906a7183d5f41f59ededdeb3
Cengiz Han <sonsair11@gmail.com>
2023-07-07 20:29:15 +0300

    Fizz Buzz

--- Dr.Chuck/Chapter3.py
+++ Dr.Chuck/Chapter3.py
@@ -1,10 +1,21 @@
 #Loops
 #Enter Pin
-print("Welcome to Bank Of Technology")
+"""print("Welcome to Bank Of Technology")
 
 pin = int(input('Enter your PIN: '))
 while pin != 1234:
   pin = int(input('Incorrect PIN. Enter your PIN again: '))
 
 if pin == 1234:
-  print('PIN accepted!')
+  print('PIN accepted!')"""
+  
+#Fizz Buzz
+for i in range(1,101):
+  if i % 3 == 0 and i % 5 == 0:
+    print("FizzBuzz")
+  elif i % 3 == 0:
+    print("Fizz")
+  elif i % 5 == 0:
+    print("Buzz")
+  else:
+    print(i)
