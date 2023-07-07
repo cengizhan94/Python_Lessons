27fc1bd57df57344b11c3661e47960a89f7b9c39
Cengiz Han <sonsair11@gmail.com>
2023-07-07 20:00:58 +0300

    Grades calculation

--- Dr.Chuck/Chapter2.py
+++ Dr.Chuck/Chapter2.py
@@ -1,9 +1,18 @@
 import random
 #Control Flow
 
-num = random.randint(0,1)
+"""num = random.randint(0,1)
 
 if num > 0.5:
     print("Picture")
 else:
-    print("Tail")
+    print("Tail")"""
+    
+#Grades Calculation
+line = 55
+
+study_grade = int(input("Study Grade: "))
+if study_grade >= line:
+    print("You Passed!")
+else:
+    print("You Failed!")
