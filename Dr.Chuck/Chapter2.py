17d82ae89d6d99d33569bb6638c90a1fedbcdca7
Cengiz Han <sonsair11@gmail.com>
2023-07-07 20:06:36 +0300

    The Cyclone

--- Dr.Chuck/Chapter2.py
+++ Dr.Chuck/Chapter2.py
@@ -25,3 +25,16 @@ elif ph <= 7:
     print("Acidic!")
 else:
     print("Natural.")
+
+#The Cyclone 
+height = int(input("What is your height?"))
+credit = int(input("What is your credit?"))
+
+if height >= 137 and credit >= 10:
+    print("Enjoy the ride!")
+elif height < 137:
+    print("You are not tall enaugh to ride.")
+elif credit < 10:
+    print("You don't have enough credits.")
+else:
+    print("Invalid data.")
