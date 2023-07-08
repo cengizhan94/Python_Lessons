7ee8286aa3170748db4eecb6218f3563762d31d1
Cengiz Han <sonsair11@gmail.com>
2023-07-08 07:25:43 +0300

    regex in defined item

--- Dr.Chuck/Chapter5/chapter5.py
+++ Dr.Chuck/Chapter5/chapter5.py
@@ -6,3 +6,9 @@ for line in hand:
     if re.search('^from',line):
         print(line)"""
 
+x = "My two favorite numbers are 23 and7"
+y = re.findall('[0-9]+',x)
+print(y)
+
+y = re.findall('[aeio]+',x)
+print(y)
