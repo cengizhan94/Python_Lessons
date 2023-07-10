02ef6516d6d27c5527a60c126b4e344843258633
Cengiz Han <sonsair11@gmail.com>
2023-07-10 15:04:52 +0300

    how to classes work?

---
+++ CS50/classes.py
@@ -0,0 +1,8 @@
+class Point():
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+p = Point(int(input("first number: ")),int(input("second number: ")))
+print(p.x)
+print(p.y)
