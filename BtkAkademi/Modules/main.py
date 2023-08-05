0799e1bc42b7884845b2009b56c5c987f45ddf40
cengizhan94 <sonsair11@gmail.com>
2023-08-05 18:08:57 +0300

    python module

--- BtkAkademi/Modules/main.py
+++ BtkAkademi/Modules/main.py
@@ -1,3 +1,10 @@
+import module
 
+# result = help(module)
+
+result = help(module.func())
+result= module.number
+result = module.numbers
+print(result)
 
 
---
+++ BtkAkademi/Modules/module.py
@@ -0,0 +1,25 @@
+
+""" 
+ module hakkında bilgilendirme
+"""
+print("module eklendi")
+
+number = 10
+
+numbers = [1,2,3]
+
+person = {
+    "name" : "Cengiz",
+    "age" : "25",
+    "city" : "Tokat"
+}
+
+def func(x):
+    """ 
+      fonksiyonlar hakkında bilgilendirme
+    """
+    print(f"x = {x}")
+
+class Person:
+    def speak(self):
+        print("speaking")
