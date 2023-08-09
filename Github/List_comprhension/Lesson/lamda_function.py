7d4e69bdbb644bc1071addcadedb388d1bee96b1
cengizhan94 <sonsair11@gmail.com>
2023-08-09 21:41:46 +0300

    LAMDA

---
+++ Github/List_comprhension/Lesson/lamda_function.py
@@ -0,0 +1,14 @@
+def add_two_numbers(a,b):
+    return a+b
+print(add_two_numbers(1,2))
+
+add_two_numbers = lambda a,b: a+b
+print(add_two_numbers(5,8))
+
+print((lambda a,b:a+b)(2,3))
+
+square = lambda x : x **2
+print(square(3))
+
+cube = lambda x : x **3
+print(cube(3))
