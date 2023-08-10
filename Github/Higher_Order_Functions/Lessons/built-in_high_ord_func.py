f1eb33b7bbdba85ce73a4de45606a01df65389cb
cengizhan94 <sonsair11@gmail.com>
2023-08-10 12:11:24 +0300

    map function

---
+++ Github/Higher_Order_Functions/Lessons/built-in_high_ord_func.py
@@ -0,0 +1,28 @@
+#Map function
+
+# numbers = [1,2,3,4,5]
+# def square(x):
+#     return x**2
+
+# numbers_squared = map(square,numbers)
+# print(list(numbers_squared))
+
+# numbers_squared2 = map(lambda x : x **2,numbers)
+# print(list(numbers_squared2))
+
+# numbers_str = ["1","2","3","4","5"]
+# numbers_int = map(int,numbers_str)
+# print(list(numbers_int))
+
+# names = ["Cengiz Han","Zeynep","Pervin"]
+
+# def change_to_upper(name):
+#     return name.upper()
+
+# names_upper_cased=map(change_to_upper,names)
+# print(list(names_upper_cased))
+
+# names_upper_cased2 = map(lambda name: name.upper(),names)
+# print(list(names_upper_cased2))
+
+#filter function
